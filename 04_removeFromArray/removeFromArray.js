c9d00559a0cf6eeab7e6df0c83e5295c97e176f2
Bart Wagener <bartwagener@gmail.com>
2024-10-22 16:41:21 +0200

    array filter and num includes methods

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+function removeFromArray(array, ...num) {
+    // Filter out the elements that match any of the 'num' values
+    return array.filter(item => !num.includes(item));
+  }
 
+  
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+//write a javascript function ccalled removeFromArray
+//it takes two arguments, array and ..num
+//this function removes an element from the array that matches num
+//for example, if num is 3, 3 will be removed from the array [1,2,3,4]
+//return what is left of the array, in this example [1,2,4]
