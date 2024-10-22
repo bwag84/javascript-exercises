c08978bc15c3043e75ecb73a0aa5d9537f295997
Bart Wagener <bartwagener@gmail.com>
2024-10-22 20:41:52 +0200

    Solved. The remainder operands is crucial for this If it can be divided by, and there is no remainder This helps to test divisibility

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,27 @@
-const leapYears = function() {
-
-};
+function leapYears(year) {
+    // Check if year is a positive integer
+    if (!Number.isInteger(year) || year <= 0) {
+      return "ERROR";
+    }
+  
+    // Leap year logic
+    if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+//write a javascript function calledd leapYears
+//that takes in an argument called year
+//check the argument to make sure it is a positive integer
+//return error if this is not the case
+//if year can be divided
+    //by 4 or 400
+        //return TRUE
+    //but if they can be divided by 100, return FALSE
+
