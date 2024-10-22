2228d996de50de4c28252e24dd8702adfc839d4b
Bart Wagener <bartwagener@gmail.com>
2024-10-22 20:49:29 +0200

    Simple calculation Learned about Math.Round You have to multiply by ten Divide by ten to get one decimal point

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
-};
+// Function to convert Fahrenheit to Celsius
+function convertToCelsius(fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5/9);
+  return Math.round(celsius * 10) / 10;  // Round to one decimal place
+}
 
-const convertToFahrenheit = function() {
-};
+// Function to convert Celsius to Fahrenheit
+function convertToFahrenheit(celsius) {
+  let fahrenheit = (celsius * (9/5)) + 32;
+  return Math.round(fahrenheit * 10) / 10;  // Round to one decimal place
+}
 
 // Do not edit below this line
 module.exports = {
