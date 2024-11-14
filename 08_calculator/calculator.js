1e6f7149b64413ae0250d40b72bb13930a2a028d
Bart Wagener <bartwagener@gmail.com>
2024-11-14 12:56:19 +0100

    Finished excersize

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,40 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+	return a - b;
+};
+
+const sum = function(a,b) {
+	//insert a sum function here for a javascript calculator. Make sure it can summ a real array of numbers
+  return a.reduce((total, current) => total + current, 0);
+
+};
+
+const multiply = function(array) {
+  //insert a function that multiplies and can take multiple arguments
+  return array.reduce((product, current) => product * current);
+};
+
+
+const power = function(a,b) {
+  return a ** b;
 	
 };
 
-const sum = function() {
-	
+//iterative factorial function, faster than recursive
+const factorial = function (n) {
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
-const multiply = function() {
 
-};
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
